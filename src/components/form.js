445a84833da9f07689e99539a711a02c8ead325c
rpinto <rachel.lea.pinto@gmail.com>
2018-08-01 12:04:12 -0700

    finished creating form with Field components

--- src/components/form.js
+++ src/components/form.js
@@ -1,45 +1,43 @@
 import React from 'react';
-import {reduxForm, Field} from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 import Input from './input';
 
 export class Form extends React.Component {
-  onSubmit(values){
+  onSubmit(values) {
     console.log(values);
   }
 
-  render(props){
+  render(props) {
     return (
       <main>
-        <div>
-          <h1>Report a problem with your delivery</h1>
-        </div>
-        <form 
-          onSubmit={this.props.handleSubmit(values => {
-          this.onSubmit(values)
-        })}
-        >
-          <div>
-            <label htmlFor='number-input'>Tracking Number</label>
-            <Field name="number-input" id="number-input" element="input" type="text" component={Input} />
-            {/* <input type='text' id='number-input' /> */}
-          </div>
-          <div>
-            <label htmlFor='issue-dropdown'>What is your issue?</label>
-            <Field name="issue-dropdown" id="issue-dropdown" element="select" component={Input}/>
-
-             {/* <select id='issue-dropdown'>
-               <option value="not-delivered">My delivery hasn't arrived</option>
-               <option value="wrong-item">The wrong item was delivered</option>
-               <option value="missing-part">Part of my order was missing</option>
-               <option value="damaged">Some of my order was damaged</option>
-               <option value="other">Other (give details below)</option>
-            </select> */}
-          </div>
+        <h1>Report a problem with your delivery</h1>
+        <form
+          onSubmit={this.props.handleSubmit(values =>
+            this.onSubmit(values)
+          )}>
+          <label htmlFor='number-input'>Tracking Number</label>
+          <Field
+            name="number-input"
+            id="number-input"
+            element="input"
+            type="text"
+            component={Input}
+          />
+          <label htmlFor='issue-dropdown'>What is your issue?</label>
+          <Field
+            name="issue-dropdown"
+            id="issue-dropdown"
+            element="select"
+            component={Input}
+          />
+          
           <label htmlFor='detail-input'>Give more details (optional)</label>
-          {/* <Field name="detail-input" id="detail-input" element="textarea" component={Input}/> */}
-           <div>
-             <textarea id='detail-input'></textarea>
-           </div>
+          <Field
+            name="detail-input"
+            id="detail-input"
+            element="textarea"
+            component={Input}
+          />    
           <button>Submit</button>
         </form>
       </main>
--- src/components/input.js
+++ src/components/input.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-export default class Input extends React.Component{
+export default class Input extends React.Component {
 
-  render(props){
+  render(props) {
     const Element = this.props.element;
 
-    return(
+
+
+    if (Element === 'select') {
+      return (
+        <select>
+          <option value="not-delivered">My delivery hasn't arrived</option>
+          <option value="wrong-item">The wrong item was delivered</option>
+          <option value="missing-part">Part of my order was missing</option>
+          <option value="damaged">Some of my order was damaged</option>
+          <option value="other">Other (give details below)</option>
+        </select>
+      )
+    }
+    return (
       <Element
         id={this.props.id}
+        
       />
     )
   }
